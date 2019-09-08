aeede133cb4b923b20ff5fc3ba759f50741129c3
linqueta <lincolnrodrs@gmail.com>
2019-09-08 13:45:20 -0300

    fix spec helper structure

--- spec/support/helpers/authentication_helper.rb
+++ spec/support/helpers/authentication_helper.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
-module Helpers
-  module AuthenticationHelper
-    def authorize
-      user = create(:user,
-                    name: 'Authentication User',
-                    email: "#{SecureRandom.uuid}test@test.com",
-                    password: SecureRandom.uuid)
-      request.headers['Authorization'] = ::Auth::JWT.encode(sub: user.id)
-    end
+module AuthenticationHelper
+  def authorize
+    user = create(:user,
+                  name: 'Authentication User',
+                  email: "#{SecureRandom.uuid}test@test.com",
+                  password: SecureRandom.uuid)
+    request.headers['Authorization'] = ::Auth::JWT.encode(sub: user.id)
   end
 end
--- spec/support/helpers/json_helper.rb
+++ spec/support/helpers/json_helper.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
-module Helpers
-  module JsonHelper
-    def body
-      { body: JSON.parse(response.body) }.deep_symbolize_keys[:body]
-    end
+module JsonHelper
+  def body
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
