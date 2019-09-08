8f19deda76896b67f73357b6c28b3ead9e529a93
linqueta <lincolnrodrs@gmail.com>
2019-09-08 00:08:58 -0300

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: "linqueta", email: 'lincolnrodrs@gmail.com', password: '123456')
