76f42cd89cb0f89b7f951ca5a8974425a80082bc
linqueta <lincolnrodrs@gmail.com>
2019-09-08 11:50:42 -0300

    prevent internal server error to expose backtrace

--- app/controllers/concerns/api_exception_handleable.rb
+++ app/controllers/concerns/api_exception_handleable.rb
@@ -6,6 +6,7 @@ module ApiExceptionHandleable
   included do
     rescue_from JWT::Authorizable::NotAuthorizedError, with: :unauthorized
     rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+    rescue_from StandardError, with: :internal_server_error
   end
 
   private
@@ -16,5 +17,9 @@ module ApiExceptionHandleable
 
   def record_invalid(error)
     render json: error, serialzer: Api::RecordInvalidSerializer, status: :unprocessable_entity
+  end
+
+  def internal_server_error
+    head :internal_server_error
   end
 end
