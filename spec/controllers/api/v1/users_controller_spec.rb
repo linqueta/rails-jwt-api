12dc9774979d77e8ab9c98e8c665f4b8259b96da
linqueta <lincolnrodrs@gmail.com>
2019-09-13 08:36:37 -0300

    add user controller update test

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -20,4 +20,32 @@ describe Api::V1::UsersController, type: :controller do
       it { expect(response.status).to eq(200) }
     end
   end
+
+  describe '#update' do
+    let(:params) { { params: { id: 1, user: { email: 'linqueta@gmail.com' } } } }
+
+    subject { patch :update, params }
+
+    before { authorization.then { subject } }
+
+    context 'without authorization' do
+      let(:authorization) { nil }
+
+      it { expect(response.status).to eq(401) }
+    end
+
+    context 'with authorization' do
+      let(:authorization) { authorize }
+
+      context 'unknown user' do
+        let(:authorization) { authorize.then { User.destroy_all } }
+
+        it { expect(response.status).to eq(401) }
+      end
+
+      context 'known user' do
+        it { expect(response.status).to eq(200) }
+      end
+    end
+  end
 end
