2f04e26c9f24eac0f850d7fbe1d06e1eedd47a15
linqueta <lincolnrodrs@gmail.com>
2019-09-13 08:07:42 -0300

    add authentication tests

--- spec/controllers/api/v1/authentication_controller_spec.rb
+++ spec/controllers/api/v1/authentication_controller_spec.rb
@@ -42,4 +42,51 @@ describe Api::V1::AuthenticationController, type: :controller do
       end
     end
   end
+
+  describe '#logout' do
+    subject { delete :logout }
+
+    before do
+      request.headers['Authorization'] = token
+      subject
+    end
+
+    context 'without token' do
+      let(:token) { nil }
+
+      it { expect(response.status).to eq(401) }
+    end
+
+    context 'with token' do
+      context 'in blacklist' do
+        let(:token) { '123'.tap { |t| ::Auth::JWT.blacklist!(t) } }
+
+        it { expect(response.status).to eq(401) }
+      end
+
+      context 'out of blacklist' do
+        context 'invalid token' do
+          let(:token) { '123' }
+
+          it { expect(response.status).to eq(401) }
+        end
+
+        context 'valid token' do
+          context 'unkown user' do
+            let(:token) { ::Auth::JWT.encode(sub: 0) }
+
+            it { expect(response.status).to eq(401) }
+          end
+
+          context 'known user' do
+            let(:user) { create :user }
+            let(:token) { ::Auth::JWT.encode(sub: user.id) }
+
+            it { expect(response.status).to eq(200) }
+            it { expect(::Auth::JWT.blacklist?(token)).to be_truthy }
+          end
+        end
+      end
+    end
+  end
 end
